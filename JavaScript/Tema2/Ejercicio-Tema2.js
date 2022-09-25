ce295d90a2a2920b39bfb852d70c83e041fb512a
Claret Devigne <57070276+claretdevigne@users.noreply.github.com>
2022-09-24 20:18:51 -0500

    Crea carpeta "Tema2"

---
+++ JavaScript/Tema2/Ejercicio-Tema2.js
@@ -0,0 +1,14 @@
+const lista = [
+    "Claret Devigne",
+    31,
+    true,
+    new Date(19, 4, 1991),
+    librofav = {
+        titulo: "Harry Potter",
+        autor: "J.K. Rowling",
+        fecha: 2009,
+        url: "No disponible"
+    }
+]
+
+console.log(lista)
