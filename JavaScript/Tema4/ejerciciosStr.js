771ff106adf9c88e56d53832f22ec124d59be7c5
Claret Devigne <57070276+claretdevigne@users.noreply.github.com>
2022-09-24 23:58:02 -0500

    Crea la carpeta "Tema4"

---
+++ JavaScript/Tema4/ejerciciosStr.js
@@ -0,0 +1,19 @@
+let nombre = "Claret";
+
+let apellido = "Devigne";
+
+let estudiante = `${nombre} ${apellido}`;
+
+let estudianteMays = estudiante.toUpperCase();
+
+let estudianteMinus = estudiante.toLowerCase();
+
+let estudianteLength = estudiante.length;
+
+let estudianteChar = nombre.charAt(0);
+
+let estudianteCharLast = apellido[apellido.length-1];
+
+let estudianteTrim = estudiante.trim();
+
+let estudianteBool = estudiante.includes(nombre);
